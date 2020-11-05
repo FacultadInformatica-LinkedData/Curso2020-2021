fa96075f8f0d8c449952e538a5c50e85793e6852
RESKOM326 <reskom326@gmail.com>
2020-11-05 21:21:22 +0100

    Minor Update: scripts added to install required libraries. PD: esta si que si jajaxd

---
+++ HandsOn/Group16/application/scripts/getlibs.sh
@@ -0,0 +1,1 @@
+pip3 install --upgrade pip; pip3 install qtmodern; pip3 install urllib3; pip3 install pyqtgraph; pip3 install PyQt5; pip3 install matplotlib; pip3 install numpy; pip3 install pandas; pip3 install requests
